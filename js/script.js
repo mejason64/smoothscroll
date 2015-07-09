b94b677805c5fafa2014b44e3ce5bc5e055ab7d7
mejason64 <evansjason64@gmail.com>
2015-07-09 09:41:41 -0700

    changes.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var timeOut;
+
+function scrollToTop() {
+	if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0){
+		window.scrollBy(0, -10);
+		timeOut = setTimeout('scrollToTop()', 10);
+
+	}else{
+		clearTimeout(timeOut);
+	}
+}
